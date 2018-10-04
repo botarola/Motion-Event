261eb7ec331b0998674b6f25354199d08ce452d7
Desarrollo4 <desarrollo4@la-Mac-mini-de-Desarrollo4.local>
2018-10-04 16:57:04 -0300

    mejora en el MotionEvent, movimiento de boton

--- app/src/main/java/com/example/desarrollo4/motionevent/MainActivity.java
+++ app/src/main/java/com/example/desarrollo4/motionevent/MainActivity.java
@@ -7,13 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+public class MainActivity extends AppCompatActivity {
 
     private Button btn1;
     private TextView tv1;
 
-    private int x;
-    private int y;
+    private float x;
+    private float y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,32 +21,35 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         setContentView(R.layout.activity_main);
         btn1 = findViewById(R.id.btn1);
-        btn1.setOnTouchListener(this);
         tv1 = findViewById(R.id.textView);
+        escucharBoton();
     }
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        x = (int)event.getX();
-        y = (int)event.getY();
-        switch (event.getAction()) {
+    private void escucharBoton(){
+        btn1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                x = event.getX();
+                y = event.getY();
+                switch (event.getAction()) {
 
-            case (MotionEvent.ACTION_DOWN):
-                tv1.setText("Pulsación DOWN, \nCordenadas: X:"+ x + ", Y:" + y);
-                break;
-            case (MotionEvent.ACTION_MOVE):
-                tv1.setText("Pulsación MOVE, \nCordenadas: X:" + x + ", Y:" + y);
-                break;
-            case (MotionEvent.ACTION_UP):
-                tv1.setText("Pulsación UP, \nCordenadas: X:" + x + ", Y:" + y);
-                break;
-            default:
-
-                break;
-
-
-        }
-        return true;
+                    case (MotionEvent.ACTION_DOWN):
+                        tv1.setText("Pulsación DOWN, \nCordenadas: X:"+ x + ", Y:" + y);
+                        break;
+                    case (MotionEvent.ACTION_MOVE):
+                        tv1.setText("Pulsación MOVE, \nCordenadas: X:" + x + ", Y:" + y);
+                        btn1.setX(event.getX()+btn1.getX()-btn1.getWidth()/2);
+                        btn1.setY(event.getY()+btn1.getY()-btn1.getHeight()/2);
+                        break;
+                    case (MotionEvent.ACTION_UP):
+                        tv1.setText("Pulsación UP, \nCordenadas: X:" + x + ", Y:" + y);
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
 }
